4d7f3031101214655fc26abcfeb7a27625e919e7
ZhNath <n.tusya@gmail.com>
2025-08-15 23:10:58 +0200

    Bugfix/131 google auth fix (#134)
    * feature: #46 add i18n
    
    * feature: #46 some translations added
    
    * feature:
    
    * to dev switch
    
    * bugfix: #131 Google Auth Fix

--- frontend/src/pages/CarPage.jsx
+++ frontend/src/pages/CarPage.jsx
@@ -2,16 +2,36 @@
 import React from 'react';
 // import usePageUrl from '../hooks/usePageUrl';
 import {useTranslation} from 'react-i18next'
+import { useState } from 'react';
+import LoaderModal from '../components/loader/LoaderModal';
 
 const CarPage = () => {
   // usePageUrl('/cars')
+  const [isLoading, setIsLoading]=useState(false)
   const {t}=useTranslation()
+  const handleOnClick=()=>{
+     setIsLoading(true)
+
+     // test
+        setTimeout(() => {
+         setIsLoading(false);
+       }, 30000); 
+
+  }
+
+
   return (
     
-    <div className="min-h-screen flex items-center justify-center bg-blue-100">
+    <div className="min-h-screen flex flex-col items-center justify-center bg-blue-100">
       <h1 className="text-4xl font-semibold text-blue-700">
         {t('carPage')}
       </h1>
+    <button
+    className='px-4 py-2 bg-blue-500 text-white rounded-md' 
+    onClick={handleOnClick}>
+    Start loader
+    </button>
+    <LoaderModal isLoading={isLoading}/>
     </div>
   );
 };
