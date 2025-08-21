164d0098cb21a27a579aca8deb1b12fd8175e23e
Oleksandr Harbuz <pamail08@gmail.com>
2025-08-21 16:55:41 +0200

    feature: #136 add changes to SwaggerConfig

--- backend/java/parser_service/src/main/java/com/voriq/parser_service/config/SwaggerConfig.java
+++ backend/java/parser_service/src/main/java/com/voriq/parser_service/config/SwaggerConfig.java
@@ -3,6 +3,10 @@ package com.voriq.parser_service.config;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 @OpenAPIDefinition(
         info = @Info(
@@ -16,5 +20,10 @@ import io.swagger.v3.oas.annotations.info.Info;
         )
 )
 
+@Configuration
 public class SwaggerConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI();
+    }
 }
