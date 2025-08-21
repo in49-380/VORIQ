5dbd377c5e694a4e650d2d8aa6507b92f5ebbbde
ZhNath <n.tusya@gmail.com>
2025-08-21 16:40:43 +0200

    Feature/157 logout 3 fix (#162)
    * feature: #157 Test-for-front
    
    * feature: #157 Test-for-front
    
    * feature: # 157 test
    
    * fix? logout
    
    * feature: #157 fix2 logout
    
    * feature: #45 fix-2
    
    * feature: #157 red-test

--- frontend/src/pages/CarPage.jsx
+++ frontend/src/pages/CarPage.jsx
@@ -30,7 +30,7 @@ const CarPage = () => {
     {/*test***********test***********test****  */}
     <Button 
       onClick={handleOnClick}
-      className='bg-yellow-500 text-black'
+      className='bg-red-500 text-black'
       children='Start Loader'
       />
 
