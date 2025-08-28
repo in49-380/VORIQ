df5d893bfdb27c549b797dbad46e9bc0ba9508a1
Natalia <n.tusya@gmail.com>
2025-08-28 22:28:56 +0200

    feature: #167-Save

--- frontend/src/components/i18n/I18nDropDown.jsx
+++ frontend/src/components/i18n/I18nDropDown.jsx
@@ -1,5 +1,5 @@
 import DropDown from "../DropDown";
-import { useEffect, useState} from "react";
+import { useState} from "react";
 import i18next from "i18next";
 import { useTranslation } from "react-i18next";
 
