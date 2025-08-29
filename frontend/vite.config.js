0f5e2a8e010d02275290be5ef24ad36c4b660e7c
Dmytro Opryshchenko <dima-opr@yandex.ru>
2025-08-29 16:05:13 +0200

    feature: #188 add swagger proxi

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
 
+  
   // Default swagger URL
   let swaggerUrl = 'http://voriq.info:8084';
 
