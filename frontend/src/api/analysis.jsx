8019d3096c67e821345c2fe584e8370276049775
Dmytro <dima-opr@yandex.ru>
2025-08-29 06:50:40 +0200

    feature/050-api-handler (#187)

---
+++ frontend/src/api/analysis.jsx
@@ -0,0 +1,21 @@
+import { get } from './api';
+
+export const analysisAPI = {
+  getVehicles({ signal }) {
+    // return [{ name: 'Mercedes-Benz' }];
+    return get('vehicle', signal);
+  },
+
+  getMeta({ signal }) {
+    return get('meta', signal);
+  },
+
+  getAnalysis({ signal }) {
+    // return [{ name: 'Analysis Data' }];
+    setTimeout(() => {
+      console.log('===ANALYSIS');
+      get('analysis?id=10', signal);
+    }, 1000);
+    // return get('analysis?id=10', signal);
+  },
+};
