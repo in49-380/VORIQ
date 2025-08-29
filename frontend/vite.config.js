a694cae6f217af18438a0d2718b40329cfe60ea9
Dmytro Opryshchenko <dima-opr@yandex.ru>
2025-08-29 13:57:07 +0200

    feature: #188 add swagger proxi

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,29 +1,34 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  //   server: {
-  //     host: '0.0.0.0',
-  //   port:3000,
-  // }
-   server: {
-    host: true,
-    allowedHosts: ["voriq.info"], 
-    port: 3000, 
-    proxy: {
-      '/swagger': {
-        target: 'http://172.17.0.8:8084/api/swagger-ui/',
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/swagger/, ''),
-      },
-      '/api/v3/api-docs': {
-        target: 'http://172.17.0.8:8084',
-        changeOrigin: true,
-        secure: false,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  // Default swagger URL
+  let swaggerUrl = 'http://172.17.0.8:8084';
+
+  if (env.VITE_SWAGGER_DOMAIN && env.VITE_SWAGGER_PORT) {
+    swaggerUrl = `${env.VITE_SWAGGER_DOMAIN}:${env.VITE_SWAGGER_PORT}`;
+  }
+
+  return {
+    plugins: [react()],
+    server: {
+      host: true,
+      allowedHosts: ["voriq.info"],
+      port: 3000,
+      proxy: {
+        '/swagger': {
+          target: swaggerUrl + '/api/swagger-ui/',
+          secure: false,
+          rewrite: (path) => path.replace(/^\/swagger/, ''),
+        },
+        '/api/v3/api-docs': {
+          target: swaggerUrl,
+          changeOrigin: true,
+          secure: false,
+        },
       },
     },
-  },
-});
+  }
+})
