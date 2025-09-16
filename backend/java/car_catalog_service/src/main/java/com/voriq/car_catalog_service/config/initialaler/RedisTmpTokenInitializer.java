1d1742a3c716fcfd49a9adccb3ee0ccdd1864071
Rslan <russen333@gmail.com>
2025-09-16 10:30:50 +0200

    hotfix: #249 add logs for redis initializer

--- backend/java/car_catalog_service/src/main/java/com/voriq/car_catalog_service/config/initialaler/RedisTmpTokenInitializer.java
+++ backend/java/car_catalog_service/src/main/java/com/voriq/car_catalog_service/config/initialaler/RedisTmpTokenInitializer.java
@@ -10,6 +10,9 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
 import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+import static java.time.Duration.ofDays;
 
 @Configuration
 @RequiredArgsConstructor
@@ -37,18 +40,22 @@ public class RedisTmpTokenInitializer {
     public ApplicationRunner initTmpTokens() {
         return args -> {
 
-            removeOldTmpToken(redisTemplate, prefix);
+            final long daysOf100Year = 36_525L;
 
-            redisTemplate.opsForValue().set(prefix + tmpToken1, "Joe Biden");
-            redisTemplate.opsForValue().set(prefix + tmpToken2, "Donald Trump");
-            redisTemplate.opsForValue().set(prefix + tmpToken3, "Barack Obama");
+            getAllRedisKeys();
+
+            removeOldTmpToken(redisTemplate, prefixOld);
+
+            redisTemplate.opsForValue().set(prefix + tmpToken1, "Joe Biden", ofDays(daysOf100Year));
+            redisTemplate.opsForValue().set(prefix + tmpToken2, "Donald Trump", ofDays(daysOf100Year));
+            redisTemplate.opsForValue().set(prefix + tmpToken3, "Barack Obama", ofDays(daysOf100Year));
             System.out.println("✅ Tokens in Redis initialized");
         };
     }
 
     public static void removeOldTmpToken(StringRedisTemplate redisTemplate, String prefix) {
 
-        Set<String> keys = redisTemplate.keys(prefixOld + "*");
+        Set<String> keys = redisTemplate.keys(prefix + "*");
         if (!keys.isEmpty()) {
             redisTemplate.delete(keys);
             log.info("Deleted {} Redis keys with prefix {}: ", keys.size(), prefix);
@@ -62,6 +69,22 @@ public class RedisTmpTokenInitializer {
         String tail = fullKey.substring(prefixOld.length());
         String first4 = tail.substring(0, Math.min(4, tail.length()));
         log.info("- {}{}****", prefixOld, first4);
+    }
+
+    private String maskKey20Char(String str) {
+        if (str.isEmpty()) return str;
+        return str.substring(0, Math.min(20, str.length())) + "****";
+    }
+
+    private void getAllRedisKeys() {
+        Set<String> keys = redisTemplate.keys("*");
+
+        keys.forEach(k -> {
+                    Long ttl = redisTemplate.getExpire(k, TimeUnit.DAYS);
+                    log.info("✅✅ Redis contains the following entries: key-{} expire-{}",
+                            maskKey20Char(k), ttl);
+                }
+        );
     }
 }
 
