22f2676e91649a06c2bbfaca1f2d9dc6de460509
Borys Pedorenko <boriszhenya@gmail.com>
2025-09-17 13:03:27 +0200

    api testing

---
+++ qa/automation/rest-api/src/main/java/org/example/CarResolve.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class CarResolve {
+    Integer idCar;
+    Integer idBrand;
+    Integer idModel;
+    Integer idYear;
+    Integer idEngine;
+    Integer idTransmission;
+    Integer idWheelDrive;
+
+
+}
--- qa/automation/rest-api/src/test/java/FirstTest.java
+++ qa/automation/rest-api/src/test/java/FirstTest.java
@@ -1,22 +1,64 @@
 import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
 import static io.restassured.RestAssured.given;
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 
 public class FirstTest extends BaseHomeWorkTest {
 
     @Test
-    public void firstTest() {
+    public void getAllBrands() {
 
         given()
                 .header("Authorization", "Bearer " + getConfig("token")).log().all()
-                .when()
-                .get(getConfig("objectBrands"))
+                .when().log().ifValidationFails()
+                .get(getConfig("objectCarCatalog") + getConfig("objectCarBrands"))
+                .then()
+                .assertThat()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body(matchesJsonSchemaInClasspath("car-schema.json"))
+                .log().ifValidationFails();
+        //.log().all();
+    }
+
+    @Test
+    public void getAllModelsByBrand() {
+
+
+        given()
+                .header("Authorization", "Bearer " + getConfig("token")).log().all()
+                .when().log().ifValidationFails()
+                .get(getConfig("objectCarCatalog") + getConfig("objectCarBrands") + "/" + get11AllBrands().get(1) + getConfig("objectCarModels"))
                 .then().assertThat()
                 .statusCode(200)
                 .contentType(ContentType.JSON)
-                //.body(matchesJsonSchemaInClasspath("schema.json"))
+                .body(matchesJsonSchemaInClasspath("car-schema.json"))
                 //.log().ifValidationFails();
                 .log().all();
+
+
     }
+
+
+
+
+
+
+
+    public List<Integer> get11AllBrands() {
+
+        Response resp = (Response) given()
+                .header("Authorization", "Bearer " + getConfig("token")).log().all()
+                .when().log().ifValidationFails()
+                .get(getConfig("objectCarCatalog") + getConfig("objectCarBrands"))
+                .then().statusCode(200)
+                .extract().response();
+        return resp.jsonPath().getList("id");
+        //.log().all();
+    }
+
 }
