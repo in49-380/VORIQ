9131a5f1363d533ec93e48f33f14c64968c4012b
Borys Pedorenko <98231709+Boriszhenya@users.noreply.github.com>
2025-09-22 05:26:22 +0200

    Test/264 automate positive tests from 71 (#265)
    * new pom
    
    * api testing
    
    * api testing
    
    * api testing 4
    
    * add 6
    
    * add 2
    
    * add 14
    
    * test: #264 Automate positive tests from in49-380/VORIQ#71

---
+++ qa/automation/rest-api/src/main/java/org/example/CarResolve.java
@@ -0,0 +1,83 @@
+package org.example;
+
+public class CarResolve {
+    private Integer brandId;
+    private Integer modelId;
+    private Integer yearId;
+    private Integer engineId;
+    private Integer transmissionId;
+    private Integer wheelDriveId;
+
+    public CarResolve(Integer brandId, Integer modelId, Integer yearId, Integer engineId, Integer transmissionId, Integer wheelDriveId) {
+        this.brandId = brandId;
+        this.modelId = modelId;
+        this.yearId = yearId;
+        this.engineId = engineId;
+        this.transmissionId = transmissionId;
+        this.wheelDriveId = wheelDriveId;
+    }
+
+    public CarResolve() {
+    }
+
+
+    public Integer getBrandId() {
+        return brandId;
+    }
+
+    public void setBrandId(Integer brandId) {
+        this.brandId = brandId;
+    }
+
+    public Integer getModelId() {
+        return modelId;
+    }
+
+    public void setModelId(Integer modelId) {
+        this.modelId = modelId;
+    }
+
+    public Integer getYearId() {
+        return yearId;
+    }
+
+    public void setYearId(Integer yearId) {
+        this.yearId = yearId;
+    }
+
+    public Integer getEngineId() {
+        return engineId;
+    }
+
+    public void setEngineId(Integer engineId) {
+        this.engineId = engineId;
+    }
+
+    public Integer getTransmissionId() {
+        return transmissionId;
+    }
+
+    public void setTransmissionId(Integer transmissionId) {
+        this.transmissionId = transmissionId;
+    }
+
+    public Integer getWheelDriveId() {
+        return wheelDriveId;
+    }
+
+    public void setWheelDriveId(Integer wheelDriveId) {
+        this.wheelDriveId = wheelDriveId;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+                "brandId = " + brandId + "," +
+                "modelId = " + modelId + "," +
+                "yearId = " + yearId + "," +
+                "engineId = " + engineId + "," +
+                "transmissionId = " + transmissionId + "," +
+                "wheelDriveId = " + wheelDriveId +
+                '}';
+    }
+}
