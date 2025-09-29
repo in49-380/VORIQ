b6d0eaf1373bd190505f05063da3fb8935cc2c74
Borys Pedorenko <98231709+Boriszhenya@users.noreply.github.com>
2025-09-29 11:54:11 +0200

    test: #280 Negative API Tests (#282)

--- qa/automation/rest-api/src/test/java/PositiveCarCatalogTest.java
+++ qa/automation/rest-api/src/test/java/PositiveCarCatalogTest.java
@@ -1,213 +1,53 @@
-import io.restassured.http.ContentType;
-import io.restassured.response.Response;
+import Utils.Service;
+import Utils.TestDataHelper;
+import io.qameta.allure.Owner;
 import org.example.CarResolve;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
-import static io.restassured.RestAssured.given;
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.Matchers.*;
 
-public class PositiveCarCatalogTest extends BaseHomeWorkTest {
+public class PositiveCarCatalogTest extends BaseApiTest {
 
 
-    @Test
+    static java.util.stream.Stream<org.junit.jupiter.params.provider.Arguments> getSuffixAndSchema() {
+        return TestDataHelper.suffixAndSchema();
+    }
+
+    @ParameterizedTest(name = "[{index}] GET {0} -> {1}")
     @Tag("positive")
-    public void getAllBrands() {
-        if (idBrand == null) {
-            Response resp = given()
-                    .header("Authorization", "Bearer " + getConfig("token"))
-                    .when().log().ifValidationFails()
-                    .get(getConfig("objectCarCatalog") + getConfig("objectCarBrands"))
-                    .then().statusCode(200)
-                    .contentType(ContentType.JSON)
-                    .body(matchesJsonSchemaInClasspath("brands-schema.json"))
-                    .extract().response();
-            idBrand = (Integer) resp.jsonPath().getList("id").get(random0toN(resp.jsonPath().getList("id").size() - 1));
-        }
+    @Owner("Borys Pedorenko")
+    @MethodSource("getSuffixAndSchema")
+    public void get_by_suffix(String suffixKeyOrLiteral, String schemaFile) {
+        apiWrapper.sendGetRequest(Service.CATALOG, resolve(suffixKeyOrLiteral))
+                .body(matchesJsonSchemaInClasspath(schemaFile));
     }
 
     @Test
     @Tag("positive")
-    public void getAllModelsByBrand() {
-        if (idBrand == null) {
-            getAllBrands();
-        }
-        if (idModel == null) {
-            Response resp = given()
-                    .header("Authorization", "Bearer " + getConfig("token"))
-                    .when().log().ifValidationFails()
-                    .get(getConfig("objectCarCatalog") + getConfig("objectCarBrands")
-                            + "/" + idBrand + getConfig("objectCarModels"))
-                    .then().statusCode(200)
-                    .contentType(ContentType.JSON)
-                    .body(matchesJsonSchemaInClasspath("models-schema.json"))
-                    .extract().response();
-            idModel = (Integer) resp.jsonPath().getList("id").get(random0toN(resp.jsonPath().getList("id").size() - 1));
-        }
-    }
-
-    @Test
-    @Tag("positive")
-    public void getAllYearsByBrandAndModel() {
-        if (idBrand == null) {
-            getAllBrands();
-        }
-        if (idModel == null) {
-            getAllModelsByBrand();
-        }
-        if (idYear == null) {
-            Response resp = given()
-                    .header("Authorization", "Bearer " + getConfig("token"))
-                    .when().log().ifValidationFails()
-                    .get(getConfig("objectCarCatalog") + getConfig("objectCarBrands")
-                            + "/" + idBrand + getConfig("objectCarModels")
-                            + "/" + idModel + getConfig("objectCarYears"))
-                    .then().statusCode(200)
-                    .contentType(ContentType.JSON)
-                    .body(matchesJsonSchemaInClasspath("years-schema.json"))
-                    .extract().response();
-            idYear = (Integer) resp.jsonPath().getList("id").get(random0toN(resp.jsonPath().getList("id").size() - 1));
-        }
-    }
-
-    @Test
-    @Tag("positive")
-    public void getAllEnginesByBrandModelAndYear() {
-        if (idBrand == null) {
-            getAllBrands();
-        }
-        if (idModel == null) {
-            getAllModelsByBrand();
-        }
-        if (idYear == null) {
-            getAllYearsByBrandAndModel();
-        }
-        if (idEngine == null) {
-            Response resp = given()
-                    .header("Authorization", "Bearer " + getConfig("token"))
-                    .when().log().ifValidationFails()
-                    .get(getConfig("objectCarCatalog") + getConfig("objectCarBrands") +
-                            "/" + idBrand + getConfig("objectCarModels") +
-                            "/" + idModel + getConfig("objectCarYears") +
-                            "/" + idYear + getConfig("objectCarEngines"))
-                    .then().statusCode(200)
-                    .contentType(ContentType.JSON)
-                    .body(matchesJsonSchemaInClasspath("engines-schema.json"))
-                    .extract().response();
-            idEngine = (Integer) resp.jsonPath().getList("id").get(random0toN(resp.jsonPath().getList("id").size() - 1));
-        }
-    }
-
-    @Test
-    @Tag("positive")
-    public void getAllTransmissionsByBrandModelYearAndEngines() {
-        if (idBrand == null) {
-            getAllBrands();
-        }
-        if (idModel == null) {
-            getAllModelsByBrand();
-        }
-        if (idYear == null) {
-            getAllYearsByBrandAndModel();
-        }
-        if (idEngine == null) {
-            getAllEnginesByBrandModelAndYear();
-        }
-        if (idTransmission == null) {
-            Response resp = given()
-                    .header("Authorization", "Bearer " + getConfig("token"))
-                    .when().log().ifValidationFails()
-                    .get(getConfig("objectCarCatalog") + getConfig("objectCarBrands") +
-                            "/" + idBrand + getConfig("objectCarModels") +
-                            "/" + idModel + getConfig("objectCarYears") +
-                            "/" + idYear + getConfig("objectCarEngines") +
-                            "/" + idEngine + getConfig("objectCarTransmissions"))
-                    .then().statusCode(200)
-                    .contentType(ContentType.JSON)
-                    .body(matchesJsonSchemaInClasspath("transmissions-schema.json"))
-                    .extract().response();
-            idTransmission = (Integer) resp.jsonPath().getList("id").get(random0toN(resp.jsonPath().getList("id").size() - 1));
-        }
-    }
-
-    @Test
-    @Tag("positive")
-    public void getAllWheelDriveByBrandModelYearEnginesANDTransmissions() {
-        if (idBrand == null) {
-            getAllBrands();
-        }
-        if (idModel == null) {
-            getAllModelsByBrand();
-        }
-        if (idYear == null) {
-            getAllYearsByBrandAndModel();
-        }
-        if (idEngine == null) {
-            getAllEnginesByBrandModelAndYear();
-        }
-        if (idTransmission == null) {
-            getAllTransmissionsByBrandModelYearAndEngines();
-        }
-        Response resp = given()
-                .header("Authorization", "Bearer " + getConfig("token"))
-                .when().log().ifValidationFails()
-                .get(getConfig("objectCarCatalog") + getConfig("objectCarBrands") +
-                        "/" + idBrand + getConfig("objectCarModels") +
-                        "/" + idModel + getConfig("objectCarYears") +
-                        "/" + idYear + getConfig("objectCarEngines") +
-                        "/" + idEngine + getConfig("objectCarTransmissions") +
-                        "/" + idTransmission + getConfig("objectCarWheelDrive"))
-                .then().statusCode(200)
-                .contentType(ContentType.JSON)
-                .body(matchesJsonSchemaInClasspath("wheel_drive-schema.json"))
-                .extract().response();
-        idWheelDrive = (Integer) resp.jsonPath().getList("id").get(random0toN(resp.jsonPath().getList("id").size() - 1));
-    }
-
-    @Test
-    @Tag("positive")
+    @Owner("Borys Pedorenko")
     public void getCarId() {
+        CarResolve carResolve = TestDataHelper.defaults(BaseApiTest::getConfig);
 
-        if (idBrand == null) {
-            getAllBrands();
-        }
-        if (idModel == null) {
-            getAllModelsByBrand();
-        }
-        if (idYear == null) {
-            getAllYearsByBrandAndModel();
-        }
-        if (idEngine == null) {
-            getAllEnginesByBrandModelAndYear();
-        }
-        if (idTransmission == null) {
-            getAllTransmissionsByBrandModelYearAndEngines();
-        }
-        if (idWheelDrive == null) {
-            getAllWheelDriveByBrandModelYearEnginesANDTransmissions();
-        }
-        CarResolve carResolve = new CarResolve(idBrand, idModel, idYear, idEngine, idTransmission, idWheelDrive);
-
-        given()
-                .contentType(ContentType.JSON).body(carResolve)
-                .header("Authorization", "Bearer " + getConfig("token"))
-                .when().log().ifValidationFails()
-                .post(getConfig("objectCarCatalog") + getConfig("objectCarResolve"))
-                .then().statusCode(200)
-                .contentType(ContentType.JSON)
-                .body(matchesJsonSchemaInClasspath("car_catalog-schema.json"));
+        apiWrapper.sendPostRequest(Service.CATALOG, resolve("objectCarResolve"), carResolve)
+                .body(matchesJsonSchemaInClasspath("car_catalog-schema.json"))
+                .body("carId", equalTo(1));
     }
+
+    int delay = 2000;
 
     @Test
     @Tag("positive")
+    @Owner("Borys Pedorenko")
     public void testController() {
-
-        given()
-                .contentType(ContentType.JSON)
-                .header("Authorization", "Bearer " + getConfig("token"))
-                .when().log().ifValidationFails()
-                .post(getConfig("objectCarCatalog") + getConfig("objectTestController"))
-                .then().statusCode(204)
-                .contentType(ContentType.JSON);
+        apiWrapper.sendGetRequestWithDelayWithoutBodyStatusCode(
+                Service.CATALOG,
+                resolve("objectTestController"),
+                delay,
+                204
+        );
     }
 }
